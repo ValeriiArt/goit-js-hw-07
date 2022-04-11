1175c619e26d1efb2a0c69545047be46cecb3d6d
Valerii Artemenko <valeraartemenko@gmail.com>
2022-04-11 03:07:21 +0300

    .

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+// console.log(galleryRef)
+
+const galleryList = galleryItems.map((image) => {
+    const { preview, original, description } = image;
+    return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="Image ${description}"
+                    />
+                </a>
+            </div>`
+}).join('');
+
+// console.log(galleryList)
+
+galleryRef.insertAdjacentHTML("beforeend", galleryList);
+
+galleryRef.addEventListener('click', clickOnImg);
+
+function clickOnImg(event) {
+    event.preventDefault();
+    const imageSource = event.target.dataset.source;
+    // console.log(imageSource);
+    const instance = basicLightbox.create(
+        `<img src="${imageSource}" width="800" height="600">`
+    );
+    instance.show();
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+// console.log(galleryRef)
+
+const galleryList = galleryItems.map((image) => {
+    const { preview, original, description } = image;
+    return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+}).join('');
+
+// console.log(galleryList)
+
+galleryRef.insertAdjacentHTML("beforeend", galleryList);
+
+var lightbox = new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt", captionPosition: 'bottom', captionDelay: 250,
+});
+
